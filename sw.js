1087d4bf103d25370c2e74aa9e98388d4b23ffa2
Lisa <lfujii.185@gmail.com>
2025-11-02 23:15:32 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+// sw.js
+const CACHE = 'shower-v1';
+const ASSETS = [
+  './',
+  './user.html',
+  './admin.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(res => res || fetch(req))
+  );
+});
